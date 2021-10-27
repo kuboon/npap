66dd7108df42ac48fae9f3e75220ec88aac3a213
Ohkubo KOHEI <kuboon@trick-with.net>
2021-10-27 13:16:32 +0000

    add og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,12 @@ declare namespace JSX {
   }
 }
 
-export default function Home () {
+export default function Home() {
   return (
     <main id='top'>
+      <head>
+        <meta property="og:image" content='https://og.kbn.one/%23%20NPAP%0ANo%20Password%0AAll%20Protected%0A%0Aパスワード不要！%0A%0A暗号鍵が埋め込まれたURLをブックマーク.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg' />
+      </head>
       <p>NPAP は PPAP を今すぐ廃止できるソリューションです。</p>
       <p>
         <a href='/NPAP'>NPAP を開く</a>
